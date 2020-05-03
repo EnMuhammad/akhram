dae61dd3a31c5ad164b7f6dc9942c3673f5a91f2
Mu7ammadBinKamal <62790611+Mu7ammadBinKamal@users.noreply.github.com>
2020-05-04 00:48:54 +0300

    IndexUpdates

--- config/index.php
+++ config/index.php
@@ -1,8 +1,12 @@
 <?php
 DEFINE('HOST', 'localhost');
-DEFINE('DB_NAME', 'akhram_new');
-DEFINE('USERNAME', 'root');
-DEFINE('PASSWORD', '');
+//DEFINE('DB_NAME', 'akhram_new'); /// Offline
+//DEFINE('USERNAME', 'root');
+//DEFINE('PASSWORD', '');
+DEFINE('DB_NAME', 'alakhram_home');//// Online
+DEFINE('USERNAME', 'alakhram_home');
+DEFINE('PASSWORD', 'alakhram102030');
+
 define('BLOCK_AFTER_TIME', '-30 minutes');
 define('MAX_TRY_TIMES', '3');
 define('ADMIN_USER', 1);
