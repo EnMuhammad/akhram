294810e918ea72c03ca19955c515ff2314c433d1
Mu7ammadBinKamal <62790611+Mu7ammadBinKamal@users.noreply.github.com>
2020-06-05 23:04:31 +0300

    HomePage - 06062020

--- pages/home.php
+++ pages/home.php
@@ -208,37 +208,36 @@ echo $style;
                                         <?php
                                     }
                                     ?>
-
-
                                 </ul>
-                            </div>
-                            <script type="text/javascript">
-                                $(window).load(function () {
-                                    $("#flexiselDemo1").flexisel({
-                                        visibleItems: 4,
-                                        animationSpeed: 1000,
-                                        autoPlay: true,
-                                        autoPlaySpeed: 3000,
-                                        pauseOnHover: true,
-                                        enableResponsiveBreakpoints: true,
-                                        responsiveBreakpoints: {
-                                            portrait: {
-                                                changePoint: 480,
-                                                visibleItems: 1
-                                            },
-                                            landscape: {
-                                                changePoint: 640,
-                                                visibleItems: 2
-                                            },
-                                            tablet: {
-                                                changePoint: 768,
-                                                visibleItems: 3
+                                <script type="text/javascript">
+                                    $(window).load(function () {
+                                        $("#flexiselDemo1").flexisel({
+                                            visibleItems: 4,
+                                            animationSpeed: 1000,
+                                            autoPlay: true,
+                                            autoPlaySpeed: 3000,
+                                            pauseOnHover: true,
+                                            enableResponsiveBreakpoints: true,
+                                            responsiveBreakpoints: {
+                                                portrait: {
+                                                    changePoint: 480,
+                                                    visibleItems: 1
+                                                },
+                                                landscape: {
+                                                    changePoint: 640,
+                                                    visibleItems: 2
+                                                },
+                                                tablet: {
+                                                    changePoint: 768,
+                                                    visibleItems: 3
+                                                }
                                             }
-                                        }
-                                    });
+                                        });
 
-                                });
-                            </script>
+                                    });
+                                </script>
+                            </div>
+
                             <script type="text/javascript" src="js/jquery.flexisel.js"></script>
                         </div>
                     </div>
